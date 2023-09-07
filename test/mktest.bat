3d4324a1ff323f781cd0f0a5b3ec5bc650970557
nino-porcino <nino.porcino@gmail.com>
2023-09-07 09:14:48 +0200

    use default header in tests

--- test/mktest.bat
+++ test/mktest.bat
@@ -5,9 +5,9 @@ call node dist/laser500wav -i test/mariel.bin -o test/mariel2 -n MARIEL2 -x --tu
 call node dist/laser500wav -i test/mariel.bin -o test/mariel3 -n MARIEL3 -x --turbo-speed 3
 call node dist/laser500wav -i test/mariel.bin -o test/mariel4 -n MARIEL4 -x --turbo-speed 4
 
-call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand0 -n AMIGAHAND0 --header 32 -t
-call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand1 -n AMIGAHAND1 --header 32 -x --turbo-speed 1
-call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand2 -n AMIGAHAND2 --header 32 -x --turbo-speed 2
-call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand3 -n AMIGAHAND3 --header 32 -x --turbo-speed 3
-call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand4 -n AMIGAHAND4 --header 32 -x --turbo-speed 4
+call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand0 -n AMIGAHAND0 -t
+call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand1 -n AMIGAHAND1 -x --turbo-speed 1
+call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand2 -n AMIGAHAND2 -x --turbo-speed 2
+call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand3 -n AMIGAHAND3 -x --turbo-speed 3
+call node dist/laser500wav -i test/amiga_hand.bin -o test/amiga_hand4 -n AMIGAHAND4 -x --turbo-speed 4
 
