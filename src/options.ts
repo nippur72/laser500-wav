d292fdd03410f9e6f79072cd839c2454bd7215d7
nino-porcino <nino.porcino@gmail.com>
2023-09-16 19:36:07 +0200

    drop --turbo-speed option, refactor options

--- src/options.ts
+++ src/options.ts
@@ -1,19 +1,32 @@
 import commandLineArgs from 'command-line-args';
 
+export interface CommandLineOptions {
+   input: string;
+   output: string;
+   samplerate: number;
+   volume: number;
+   stereoboost: boolean;
+   invert: boolean;
+   header: number;
+   pulsems: number;
+   turbo: number;   
+   l500: boolean;
+   l310: boolean;
+}
+
 export const options = parseOptions([
-   { name: 'input', alias: 'i', type: String, defaultOption: true },
+   { name: 'input', alias: 'i', type: String,  },
    { name: 'output', alias: 'o', type: String },
-   { name: 'samplerate', alias: 's', type: Number },
-   { name: 'volume', alias: 'v', type: Number },
-   { name: 'stereoboost', type: Boolean },
-   { name: 'invert', type: Boolean },
-   { name: 'header', type: Number },
-   { name: 'pulsems', type: Number },
-   { name: 'turbo', alias: 'x', type: Boolean },
-   { name: 'turbo-speed', type: Number },
+   { name: 'samplerate', alias: 's', type: Number, defaultValue: 96000 },
+   { name: 'volume', alias: 'v', type: Number, defaultValue: 1 },
+   { name: 'stereoboost', type: Boolean, defaultValue: false },
+   { name: 'invert', type: Boolean, defaultValue: false },
+   { name: 'header', type: Number, defaultValue: 128 },
+   { name: 'pulsems', type: Number, defaultValue: 277 },
+   { name: 'turbo', alias: 'x', type: Number, defaultValue: 0 },
    { name: 'l500', type: Boolean },
    { name: 'l310', type: Boolean }
-]);
+]) as CommandLineOptions;
 
 function parseOptions(optionDefinitions: commandLineArgs.OptionDefinition[]) {
     try {
@@ -24,5 +37,3 @@ function parseOptions(optionDefinitions: commandLineArgs.OptionDefinition[]) {
        process.exit(-1);
     }
 }
-
-
--- test/mktest.bat
+++ test/mktest.bat
@@ -4,39 +4,39 @@ rem call node dist/vzpack -i test/Laser500/mariel.bin     -o test/Laser500/marie
 rem call node dist/vzpack -i test/Laser500/amiga_hand.bin -o test/Laser500/amiga_hand.vz -n AMIGAHAND -t --address 8995
 rem 
 rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel0
-rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel1 -x --turbo-speed 1
-rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel2 -x --turbo-speed 2
-rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel3 -x --turbo-speed 3
-rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel4 -x --turbo-speed 4
+rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel1 --turbo 1
+rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel2 --turbo 2
+rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel3 --turbo 3
+rem call node dist/laser500wav --l500 -i test/Laser500/mariel.vz -o test/Laser500/mariel4 --turbo 4
 rem 
 rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand0
-rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand1 -x --turbo-speed 1
-rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand2 -x --turbo-speed 2
-rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand3 -x --turbo-speed 3
-rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand4 -x --turbo-speed 4
+rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand1 --turbo 1
+rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand2 --turbo 2
+rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand3 --turbo 3
+rem call node dist/laser500wav --l500 -i test/Laser500/amiga_hand.vz -o test/Laser500/amiga_hand4 --turbo 4
 
 call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello0
-call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello1 -x --turbo-speed 1
-call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello2 -x --turbo-speed 2
-call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello3 -x --turbo-speed 3
-call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello4 -x --turbo-speed 4
+call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello1 --turbo 1
+call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello2 --turbo 2
+call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello3 --turbo 3
+call node dist/laser500wav --l310 -i test/Laser310/hello.vz -o test/Laser310/hello4 --turbo 4
 
 call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker0
-call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker1 -x --turbo-speed 1
-call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker2 -x --turbo-speed 2
-call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker3 -x --turbo-speed 3
-call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker4 -x --turbo-speed 4
+call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker1 --turbo 1
+call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker2 --turbo 2
+call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker3 --turbo 3
+call node dist/laser500wav --l310 -i test/Laser310/test_speaker.vz -o test/Laser310/test_speaker4 --turbo 4
 
 call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris0
-call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris1 -x --turbo-speed 1
-call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris2 -x --turbo-speed 2
-call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris3 -x --turbo-speed 3
-call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris4 -x --turbo-speed 4 
-call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris4F -x --turbo-speed 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris1 --turbo 1
+call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris2 --turbo 2
+call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris3 --turbo 3
+call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris4 --turbo 4 
+call node dist/laser500wav --l310 -i test/Laser310/tetris.vz -o test/Laser310/tetris4F --turbo 4 --header 64 --pulsems 270
 
-call node dist/laser500wav --l310 -i test/Laser310/circus.vz   -o test/Laser310/circus   -x --turbo-speed 4 --header 64 --pulsems 270
-call node dist/laser500wav --l310 -i test/Laser310/crash.vz    -o test/Laser310/crash    -x --turbo-speed 4 --header 64 --pulsems 270
-call node dist/laser500wav --l310 -i test/Laser310/invaders.vz -o test/Laser310/invaders -x --turbo-speed 4 --header 64 --pulsems 270
-call node dist/laser500wav --l310 -i test/Laser310/vz_cave.vz  -o test/Laser310/vz_cave  -x --turbo-speed 4 --header 64 --pulsems 270
-call node dist/laser500wav --l310 -i test/Laser310/dawn.vz     -o test/Laser310/dawn     -x --turbo-speed 4 --header 64 --pulsems 270
-call node dist/laser500wav --l310 -i test/Laser310/kamikaze.vz -o test/Laser310/kamikaze -x --turbo-speed 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/circus.vz   -o test/Laser310/circus   --turbo 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/crash.vz    -o test/Laser310/crash    --turbo 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/invaders.vz -o test/Laser310/invaders --turbo 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/vz_cave.vz  -o test/Laser310/vz_cave  --turbo 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/dawn.vz     -o test/Laser310/dawn     --turbo 4 --header 64 --pulsems 270
+call node dist/laser500wav --l310 -i test/Laser310/kamikaze.vz -o test/Laser310/kamikaze --turbo 4 --header 64 --pulsems 270
